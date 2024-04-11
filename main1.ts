f0a7f7cd03f0b3403a5bd51554ba596f4d61dc85
Faryal88 <faryalabbasi88@gmail.com>
2024-04-11 21:27:23 +0500

    Adding a new currency converter

---
+++ main1.ts
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.blue.bold("\n \t Welcome to 'Faryal Abbasi' = Currency C onvertor\n"));
+
+let currency: any = {
+	USD: 1,
+	EUR: 0.91,
+	GBP: 0.76,
+	INR: 74.57,
+	PKR: 280,
+};
+
+let user_answer = await inquirer.prompt(
+	[
+		{
+			name: 'from',
+			message: "Enter From Currency",
+			type: 'list',
+			choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+		},
+		{
+			name: 'to',
+			message: "Enter To Currency",
+			type: 'list',
+			choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+		},
+		{
+			name: 'amount',
+			message: "Enter From Amount",
+			type: 'number',
+		}
+	]
+)
+
+let from_amount = currency[user_answer.from]
+let to_amount = currency[user_answer.to]
+let amount = user_answer.amount
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+console.log(converted_amount);
+
+
+
+
+
+
